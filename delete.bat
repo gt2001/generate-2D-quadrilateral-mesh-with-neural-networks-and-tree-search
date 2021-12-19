348af9d971f7fc5bdac9dfadfbaa12b2304e1469
Hua Tong <78462088+gt2001@users.noreply.github.com>
2021-12-19 23:15:35 +0800

    Update delete.bat

--- delete.bat
+++ delete.bat
@@ -1,4 +1,9 @@
 del weight.w
 del weight_beyond.w
+del record_loss.txt
+del record_round_max_quality.txt
+del record_test.txt
+del record_time.txt
+del record_train.txt
 rmdir /s /q results
 rmdir /s /q results-ls
